36009f2af0492ae9d3966ae85cf2b6edcc0cf6ba
Oleg <olegbilas@gmail.com>
2023-11-27 14:37:50 +0200

    Update 5.ts

--- src/generic/5.ts
+++ src/generic/5.ts
@@ -3,9 +3,9 @@
   Використовуйте generics, щоб цей інтерфейс міг працювати з будь-якими типами ключів та значень.
 */
 
-interface KeyValuePair<T, K> {
+interface KeyValuePair<T> {
   key: T;
-  value: K;
+  value: T;
 }
 
 export {};
